a4611362137e168eb7e3f6f3d11f1f70cd240543
Ranjit Sengupta <165263290+ranjit1965@users.noreply.github.com>
2024-09-28 16:55:58 +0530

    mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -1,5 +1,5 @@
 chmod 777 mongo.sh
-pip install -y requirements.txt
+pip install -r requirements.txt
 mkdir composs
 cd composs
 sudo dnf install -y https://repo.mongodb.org/yum/redhat/8/mongodb-org/4.4/x86_64/RPMS/mongodb-org-server-4.4.6-1.el8.x86_64.rpm
