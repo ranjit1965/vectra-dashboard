f9ba6fc4508c2b6176250234e0a7b4a5d6abbc02
Ranjit Sengupta <165263290+ranjit1965@users.noreply.github.com>
2024-09-28 16:48:46 +0530

    Create mongo.sh

---
+++ mongo.sh
@@ -0,0 +1,11 @@
+chmod 777 mongo.sh
+mkdir composs
+cd composs
+sudo dnf install -y https://repo.mongodb.org/yum/redhat/8/mongodb-org/4.4/x86_64/RPMS/mongodb-org-server-4.4.6-1.el8.x86_64.rpm
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+wget https://downloads.mongodb.com/compass/mongodb-compass-1.43.5.x86_64.rpm
+sudo yum install mongodb-compass-1.43.5.x86_64.rpm -y
+
+mongodb-compass 
